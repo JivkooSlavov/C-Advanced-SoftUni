3dbbf282cad126ae09c7d02e77be718f1af78112
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-01-25 15:29:38 +0200

    Add files via upload

---
+++ 3.StacksAndQueues/08.TrafficLight/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _08.TrafficLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int numbersOfCars = int.Parse(Console.ReadLine());
+           Queue<string> cars = new Queue<string>();
+            int passedCars = 0;
+            string command = "";
+            while ((command = Console.ReadLine())!="end")
+            {
+                if (command =="green")
+                {
+                    for (int i = 0; i < numbersOfCars; i++)
+                    {
+                        if (cars.Count==0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        passedCars++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
