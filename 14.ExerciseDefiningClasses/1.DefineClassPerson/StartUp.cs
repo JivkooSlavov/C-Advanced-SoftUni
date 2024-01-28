a0e16e86808848ab078c944b20a394a4b5cd4a22
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-01-28 17:35:10 +0200

    Add files via upload

---
+++ 14.ExerciseDefiningClasses/1.DefineClassPerson/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Peter", 20);
+            Person person1 = new Person();
+            person1.Name = "Pesho";
+            person1.Age = 18;
+            
+
+        }
+    }
+}
