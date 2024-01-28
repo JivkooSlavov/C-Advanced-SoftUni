9e92da08d85f395c06b33c6ddbbb93f32a991638
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-01-28 17:35:37 +0200

    Add files via upload

---
+++ 14.ExerciseDefiningClasses/2.CreatingConstructors/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person person1 = new Person(24);
+            Person person2 = new Person("Zhivko", 33);  
+        }
+    }
+}
