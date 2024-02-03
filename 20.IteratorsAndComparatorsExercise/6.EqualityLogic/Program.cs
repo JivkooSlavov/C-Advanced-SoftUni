bf7c87baad7bc62e6ceb5f407f9f5da3c780a5b0
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-03 09:49:59 +0200

    Add files via upload

---
+++ 20.IteratorsAndComparatorsExercise/6.EqualityLogic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6.EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> hashSet = new HashSet<Person>();
+            int countOfPeople = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                string[] informationForPerson = Console.ReadLine().Split().ToArray();
+                string name = informationForPerson[0];
+                int age = int.Parse(informationForPerson[1]);
+                Person person = new Person(name, age);
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
