8b23aece89528d97633726c7d4ebd309156294d2
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-04 18:18:24 +0200

    Add files via upload

---
+++ 7.SetsAndDictionariesAdvanced/5.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _5.CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfCountries = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> register = new();
+                
+            for (int i = 0; i < countOfCountries; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string continent = input[0];
+                string country = input[1];
+                string town = input[2];
+                if (!register.ContainsKey(continent))
+                {
+                    register.Add(continent, new Dictionary<string,List<string>>());
+                }
+                if (!register[continent].ContainsKey(country))
+                {
+                    register[continent].Add(country, new List<string>());
+                }
+                register[continent][country].Add(town);
+            }
+            foreach (var item in register)
+            {
+                Console.WriteLine($"{item.Key}:");
+                foreach (var value in item.Value)
+                {
+                    Console.WriteLine($"   {value.Key} -> {string.Join(", ", value.Value)}");
+                }
+            }
+        }
+    }
+}
