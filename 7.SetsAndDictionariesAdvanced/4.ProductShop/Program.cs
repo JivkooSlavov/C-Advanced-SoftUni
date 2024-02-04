f467cbeae4673aabd38f6b6e218d4a8ae516bbfc
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-04 18:17:46 +0200

    Add files via upload

---
+++ 7.SetsAndDictionariesAdvanced/4.ProductShop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace _4.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+            string command = "";
+            while ((command=Console.ReadLine())!= "Revision")
+            {
+                string[] array = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string nameOfShop = array[0];
+                string nameOfProduct = array[1];
+                double priceOfProduct = double.Parse(array[2]);
+
+                if (!shops.ContainsKey(nameOfShop))
+                {
+                    shops.Add(nameOfShop, new Dictionary<string, double>());
+                }
+                shops[nameOfShop].Add(nameOfProduct, priceOfProduct);
+
+            }
+            foreach (var shop in shops.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var item in shop.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ 7.SetsAndDictionariesAdvanced/8.SoftUniParty/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _8.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guests = new();
+            HashSet<string> guestsArrived = new();
+            string command = "";
+            while ((command=Console.ReadLine())!= "END")
+            {
+                if (command=="PARTY")
+                {
+                    string invited = Console.ReadLine();
+                    guestsArrived.Add(invited);
+                    continue;
+                }
+                guests.Add(command);
+            }
+            guests.IntersectWith(guestsArrived);
+
+            foreach (var item in guests)
+            {
+                Console.WriteLine(guests.Count);
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
