f9c4002522b6a29aa4b9c3751f239411a9cbf262
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-04 18:18:07 +0200

    Add files via upload

---
+++ 7.SetsAndDictionariesAdvanced/6.RecordUniqueNames/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _6.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNames = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 7.SetsAndDictionariesAdvanced/7.ParkingLot/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet <string> collection = new HashSet<string>();
+
+            string input = "";
+            while ((input =Console.ReadLine())!="END")
+            {
+                string[] array = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string operation = array[0];
+                string number = array[1];
+                if (operation == "IN")
+                {
+                    collection.Add(number);
+                }
+                else
+                {
+                    collection.Remove(number);
+                }
+            }
+            if (collection.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+            foreach (string item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
