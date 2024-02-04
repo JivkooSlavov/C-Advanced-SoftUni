120ea145b67ccb3219c3a1119e863ca84c2a5a86
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-04 18:11:42 +0200

    Add files via upload

---
+++ ExamPreparation/ExamPreparation.December2021/1.Blacksmith/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.Blacksmith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> steel = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> carbon = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            int totalSwords = 0;
+
+
+            SortedDictionary<string, int> swords = new SortedDictionary<string, int>();
+            swords.Add("Gladius", 0);
+            swords.Add("Shamshir", 0);
+            swords.Add("Katana", 0);
+            swords.Add("Sabre", 0);
+            swords.Add("Broadsword", 0);
+
+            while (steel.Count > 0 && carbon.Count > 0)
+            {
+                int currentSteel = steel.Peek();
+                int currentCarbon = carbon.Peek();
+                int sumOfSteelAndCarbon = currentCarbon + currentSteel;
+
+                if (sumOfSteelAndCarbon == 70)
+                {
+                    swords["Gladius"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                    totalSwords++;
+                }
+                else if (sumOfSteelAndCarbon == 80)
+                {
+                    swords["Shamshir"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                    totalSwords++;
+                }
+                else if (sumOfSteelAndCarbon == 90)
+                {
+                    swords["Katana"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                    totalSwords++;
+                }
+                else if (sumOfSteelAndCarbon == 110)
+                {
+                    swords["Sabre"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                    totalSwords++;
+                }
+                else if (sumOfSteelAndCarbon == 150)
+                {
+                    swords["Broadsword"]++;
+                    steel.Dequeue();
+                    carbon.Pop();
+                    totalSwords++;
+                }
+                else
+                {
+                    steel.Dequeue();
+                    currentCarbon += 5;
+                    carbon.Pop();
+                    carbon.Push(currentCarbon);
+                }
+            }
+            if (totalSwords > 0)
+            {
+                Console.WriteLine($"You have forged {totalSwords} swords.");
+            }
+            else
+            {
+                Console.WriteLine($"You did not have enough resources to forge a sword.");
+            }
+            if (steel.Count == 0)
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            else
+            {
+                Console.WriteLine("Steel left: " + string.Join(", ", steel));
+            }
+            if (carbon.Count == 0)
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            else
+            {
+                Console.WriteLine("Carbon left: " + string.Join(", ", carbon));
+            }
+
+            foreach (var sword in swords.OrderBy(sword => sword.Key))
+            {
+                if (sword.Value != 0)
+                {
+                    Console.WriteLine($"{sword.Key}: {sword.Value}");
+                }
+
+            }
+        }
+    }
+}
---
+++ ExamPreparation/ExamPreparation.December2021/2.TruffleHunter/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.TruffleHunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = rows;
+
+            string[,] matrix = new string[rows, cols];
+            int blackTruffel = 0;
+            int whiteTruffel = 0;
+            int summerTruffel = 0;
+            int truffelEatenByBoar = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                string[] rowValue = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowValue[col];
+                }
+            }
+            string input = "";
+            while ((input = Console.ReadLine()) != "Stop the hunt")
+            {
+                string[] command = input.Split().ToArray();
+                string action = command[0];
+                int rowPosition = int.Parse(command[1]);
+                int colPosition = int.Parse(command[2]);
+                if (action == "Collect")
+                {
+                    if (matrix[rowPosition, colPosition] == "S")
+                    {
+                        summerTruffel++;
+                        matrix[rowPosition, colPosition] = "-";
+                    }
+                    else if (matrix[rowPosition, colPosition] == "B")
+                    {
+                        blackTruffel++;
+                        matrix[rowPosition, colPosition] = "-";
+                    }
+                    else if (matrix[rowPosition, colPosition] == "W")
+                    {
+                        whiteTruffel++;
+                        matrix[rowPosition, colPosition] = "-";
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                if (action == "Wild_Boar")
+                {
+                    string direction = command[3];
+                        if (direction == "up")
+                        {
+                        while (IsValidRow(rowPosition, rows))
+                        {
+                            if (matrix[rowPosition, colPosition] == "S" || matrix[rowPosition, colPosition] == "B" || matrix[rowPosition, colPosition] == "W")
+                            {
+                                truffelEatenByBoar++;
+                                matrix[rowPosition, colPosition] = "-";
+                                rowPosition -= 2;
+                            }
+                            else
+                            {
+                                rowPosition -= 2;
+                            }
+                        }
+                        }
+                        else if (direction == "down")
+                        {
+                            while (IsValidRow(rowPosition, rows))
+                            {
+                            if (matrix[rowPosition, colPosition] == "S" || matrix[rowPosition, colPosition] == "B" || matrix[rowPosition, colPosition] == "W")
+                            {
+                                truffelEatenByBoar++;
+                                matrix[rowPosition, colPosition] = "-";
+                                rowPosition += 2;
+                            }
+                            else
+                            {
+                                rowPosition += 2;
+                            }
+                        }
+                        }
+                        else if (direction == "left")
+                        {
+                            while (IsValidRow(colPosition, cols))
+                            {
+                            if (matrix[rowPosition, colPosition] == "S" || matrix[rowPosition, colPosition] == "B" || matrix[rowPosition, colPosition] == "W")
+                            {
+                                truffelEatenByBoar++;
+                                matrix[rowPosition, colPosition] = "-";
+                                colPosition -= 2;
+                            }
+                            else
+                            {
+                                colPosition -= 2;
+                            }
+                        }
+                        }
+                        else if (direction == "right")
+                        {
+                            while (IsValidRow(colPosition, cols))
+                            {
+                            if (matrix[rowPosition, colPosition] == "S" || matrix[rowPosition, colPosition] == "B" || matrix[rowPosition, colPosition] == "W")
+                            {
+                                truffelEatenByBoar++;
+                                matrix[rowPosition, colPosition] = "-";
+                                colPosition += 2;
+                            }
+                            else
+                            {
+                                colPosition += 2;
+                            }
+                        }
+                        }
+                }
+
+            }
+            Console.WriteLine($"Peter manages to harvest {blackTruffel} black, {summerTruffel} summer, and {whiteTruffel} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {truffelEatenByBoar} truffles.");
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool IsValidRow(int row, int rows)
+        {
+            return row >= 0 && row < rows;
+        }
+        public static bool IsValidCol(int col, int cols)
+        {
+            return col >= 0 && col < cols;
+        }
+    }
+}
