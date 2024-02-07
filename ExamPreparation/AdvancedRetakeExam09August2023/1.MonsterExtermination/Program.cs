f2dd9230c41157136a4616b9b058c42405cb10db
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-07 08:45:06 +0200

    Add files via upload

---
+++ ExamPreparation/AdvancedRetakeExam09August2023/1.MonsterExtermination/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualBasic;
+
+Queue<int> armorsOfMonster = new Queue<int>(Console.ReadLine().Split(",").Select(int.Parse).ToArray());
+Stack<int> strikesOfSoldier = new Stack<int>(Console.ReadLine().Split(",").Select(int.Parse).ToArray());
+
+int killedMonster = 0;
+
+while (armorsOfMonster.Count > 0 && strikesOfSoldier.Count > 0)
+{
+    int armor = armorsOfMonster.Peek();
+    int strike = strikesOfSoldier.Peek();
+    if (strike >= armor)
+    {
+        killedMonster++;
+        strike -= armor;
+        if (strike == 0)
+        {
+            strikesOfSoldier.Pop();
+            armorsOfMonster.Dequeue();
+        }
+        else
+        {
+            armorsOfMonster.Dequeue();
+            if (strikesOfSoldier.Count == 1)
+            {
+                strikesOfSoldier.Pop();
+                strikesOfSoldier.Push(strike);
+                continue;
+            }
+            else
+            {
+                strikesOfSoldier.Pop();
+                int tempStrike = strike;
+                strikesOfSoldier.Push(strikesOfSoldier.Pop() + tempStrike);
+            }
+        }
+    }
+    else
+    {
+        int leftArmor = armor - strike;
+        strikesOfSoldier.Pop();
+        armorsOfMonster.Dequeue();
+        armorsOfMonster.Enqueue(leftArmor);
+    }
+}
+if (armorsOfMonster.Count == 0)
+{
+    Console.WriteLine("All monsters have been killed!");
+}
+if (strikesOfSoldier.Count == 0)
+{
+    Console.WriteLine("The soldier has been defeated.");
+}
+Console.WriteLine($"Total monsters killed: {killedMonster}");
