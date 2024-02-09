b9dba75663d86328c0fa1955a7a87354a12abbab
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-09 21:52:13 +0200

    Add files via upload

---
+++ 09.StreamsFilesAndDirectories/1.OddLines/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int lineNumber = 0;
+                    while (reader.EndOfStream == false)
+                    {
+                        string line = reader.ReadLine();
+                        lineNumber++;
+                        if (lineNumber % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ 09.StreamsFilesAndDirectories/test/Program.cs
@@ -0,0 +1,19 @@
+﻿EncryptFile("../../../EncryptFile.jpg");
+void EncryptFile (string fileName)
+{
+    using(FileStream stream = new FileStream(fileName, FileMode.OpenOrCreate))
+    {
+        byte[] data = new byte[stream.Length];
+        stream.Read(data, 0, data.Length);
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = (byte)(data[i] ^ 104);
+        }
+
+        stream.Seek(0, SeekOrigin.Begin);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine(data.Length);
+        Console.WriteLine(string.Join(",", data));
+    }
+}
