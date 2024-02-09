86f4a394f2fad5d939b9e252badab84c621bc900
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-09 21:53:35 +0200

    Add files via upload

---
+++ 08.ExerciseSetsAndDictionariesAdvanced/1.UniqueUsernames/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new();
+            int counts = int.Parse(Console.ReadLine());
+            for (int i = 0; i < counts; i++)
+            {
+                string user = Console.ReadLine();
+                usernames.Add(user);
+            }
+            foreach (var item in usernames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
