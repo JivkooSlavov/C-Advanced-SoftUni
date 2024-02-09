66a9101350c94bfd90ede6d05c0b5d48c70c4752
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-09 21:54:29 +0200

    Add files via upload

---
+++ 08.ExerciseSetsAndDictionariesAdvanced/3.PeriodicTable/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _3.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> periodicElements = new();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                periodicElements.UnionWith(tokens);
+            }
+            Console.WriteLine(string.Join(" ", periodicElements));
+        }
+    }
+}
