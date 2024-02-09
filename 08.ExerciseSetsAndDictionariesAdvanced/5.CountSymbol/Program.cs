bf679e9de5f409c8f3f711bcccf2b471706428b9
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-09 21:56:14 +0200

    Add files via upload

---
+++ 08.ExerciseSetsAndDictionariesAdvanced/5.CountSymbol/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _5.CountSymbol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> charsCounts = new SortedDictionary<char, int>();
+
+            string input =Console.ReadLine();
+
+            foreach (char ch in input)
+            {
+                if (!charsCounts.ContainsKey(ch))
+                {
+                    charsCounts.Add(ch, 0);
+                }
+                charsCounts[ch]++;
+            }
+            foreach (var item in charsCounts)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
