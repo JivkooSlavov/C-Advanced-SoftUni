a5a0f6ab2e601578ec7818897cfa7bf308e8cc75
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-15 08:48:02 +0200

    Add files via upload

---
+++ 12.ExerciseFunctionalProgramming/11.TriFunction/Program.cs
@@ -0,0 +1,5 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<string> people = Console.ReadLine().Split().ToList();
+
+Console.WriteLine(people
+    .First(name => name.Select(symbol => (int)symbol).Sum() >= n));
