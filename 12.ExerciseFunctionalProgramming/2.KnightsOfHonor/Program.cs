03102949e0a6dde1fe89e704cfbed2c4aa63141c
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-15 08:46:18 +0200

    Add files via upload

---
+++ 12.ExerciseFunctionalProgramming/2.KnightsOfHonor/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Channels;
+
+List <string> knights = Console.ReadLine().Split().ToList();
+
+Func<string, string> addPrefix = name => "Sir" + name;
+
+foreach (var name in knights)
+{
+    Console.WriteLine(addPrefix(name));
+}
