1efde6a29009be788ff74c2fd1c1e2ee2109bf2b
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-15 08:47:34 +0200

    Add files via upload

---
+++ 12.ExerciseFunctionalProgramming/8.ListOfPredicates/Program.cs
@@ -0,0 +1,30 @@
+﻿int n  =int.Parse(Console.ReadLine());  
+List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> numbers = new();
+for (int i = 1; i <= n; i++)
+{
+    numbers.Add(i);
+}
+
+List<int> printNumbers = new(); 
+
+foreach (var number in numbers)
+{
+	bool isDivisible = true;
+	foreach (var divider in dividers)
+	{
+		Predicate<int> divisible = number => number % divider == 0;
+
+		if (!divisible(number))
+		{
+			isDivisible = false;
+			break;
+		}
+	}
+	if (isDivisible)
+	{
+		printNumbers.Add(number);
+	}
+}
+Console.WriteLine(string.Join(" ", printNumbers));
