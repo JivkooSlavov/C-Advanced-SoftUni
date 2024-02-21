355f6ee96bb3e31a7667bfc098bfd3e9dfc0f1b5
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-21 10:49:50 +0200

    Add files via upload

---
+++ 11.FunctionalProgramming/4.AddVAT/Program.cs
@@ -0,0 +1,6 @@
+﻿List<double> numbers = Console.ReadLine().Split(", ").Select(double.Parse).Select(x=>x*1.2).ToList();
+
+foreach (var number in numbers)
+{
+    Console.WriteLine($"{number:f2}");
+}
