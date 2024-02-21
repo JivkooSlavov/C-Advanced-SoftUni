b06ad0b0317656591602f546dd7bc8e9a2a21441
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-02-21 10:49:15 +0200

    Add files via upload

---
+++ 11.FunctionalProgramming/2.SumNumbers/Program.cs
@@ -0,0 +1,5 @@
+﻿string input = Console.ReadLine();
+Func<string, int> parse = x => int.Parse(x);
+int[] array = input.Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries).Select(parse).ToArray();
+Console.WriteLine(array.Length);
+Console.WriteLine(array.Sum());
