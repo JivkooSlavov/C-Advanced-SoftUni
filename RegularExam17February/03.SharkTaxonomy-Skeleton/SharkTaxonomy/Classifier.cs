a1153484c48c71b40bc3284b6bab9d1933446f87
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-03-02 18:15:20 +0200

    Add files via upload

---
+++ RegularExam17February/03.SharkTaxonomy-Skeleton/SharkTaxonomy/Classifier.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+        public Classifier(int capacity)
+        {
+            Capacity = capacity;
+            Species = new List<Shark>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Shark> Species { get; set; }
+
+        public int GetCount =>Species.Count;
+
+        public void AddShark(Shark shark)
+        {
+            if (Capacity > Species.Count)
+            {
+                if (!Species.Contains(shark))
+                {
+                    Species.Add(shark);
+                }
+            }
+        }
+        public bool RemoveShark(string kind) => Species.Remove(Species.FirstOrDefault(x => x.Kind == kind));
+
+        public string GetLargestShark() => Species.OrderByDescending(y => y.Length).FirstOrDefault().ToString();
+
+        public double GetAverageLength() => Species.Average(z => z.Length);
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Species.Count} sharks classified:");
+            foreach (var shark in Species)
+            {
+                sb.AppendLine(shark.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
